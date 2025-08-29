6adbde31fbf7df4a5099f761abe5cdf531a1e267
Taiwo <taiwoayimora623@gmail.com>
2025-08-29 19:42:18 +0100

    upated savings

--- Bank_core/savings.py
+++ Bank_core/savings.py
@@ -20,3 +20,20 @@ class Savings_Account():
         else:
             self.balance += amount
             return self.balance
+        
+    def apply_interest(self):
+        interest = self.balance * self.interest_rate / 100
+        self.balance += interest
+        return self.balance
+    
+    def check_balance(self):
+        if self.balance < 1000:
+            raise ValueError("Minimum balance of 1000 is required")
+        else:
+            return self.balance
+        
+    def __str__(self):
+        return f"Savings Account({self.account_number}, {self.account_holder}, {self.interest_rate}%, Balance: {self.balance})"
+    
+    def __repr__(self):
+        return f"Savings_Account({self.account_number}, {self.account_holder}, {self.interest_rate}%, {self.balance})"
